31a7ce1af31d6565e69f3bfd34d2cfd2bd1fc157
Vitja-Nosa <vitjanosa5@hotmail.com>
2025-09-21 12:18:14 +0200

    minor fixes

--- src/db/sql/connection.js
+++ src/db/sql/connection.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'db-mysql-ams3-46626-do-user-8155278-0.b.db.ondigitalocean.com',
-    user: process.env.DB_USER || '2230173',
-    password: process.env.DB_PASSWORD || 'geheim2',
-    database: process.env.DB_DATABASE || '2230173',
-    port: process.env.DB_PORT || 25060,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     multipleStatements: true,
     waitForConnections: true,
     connectionLimit: 10,
